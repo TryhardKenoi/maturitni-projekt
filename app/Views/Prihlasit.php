35af803943730cfd6e7ed45c9c63531648b355db
Kubka <kenoicz@gmail.com>
2023-11-29 18:11:15 +0100

    Create Prihlasit.php

---
+++ app/Views/Prihlasit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+  <title>Kenoi's website</title>
+    <link type="text/css" rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
+    <link type="text/css" rel="stylesheet" href="<?= base_url('assets/bootstrap/css/custom.css'); ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <meta charset='utf-8' />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@6.1.9/index.global.min.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+  </head>
+  <body>
+
+    <?php include_once 'vendor\benedmunds\codeigniter-ion-auth\Views\auth\login.php'; ?>
+
+  </body>
+</html>
